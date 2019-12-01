5dcf0f0fd7cfab16dc0e2a870bd5fcf1e955fb3b
Rene-Alexandre Giroux <ragiroux@gmail.com>
2019-12-01 11:42:47 -0500

    advent1 part2

--- advent1/lib/advent1.ex
+++ advent1/lib/advent1.ex
@@ -1,5 +1,8 @@
 defmodule Advent1 do
 
+  ############
+  # PART 1
+  ############
   def main do
     readFile
     |> Enum.map(&IO.inspect(&1))
@@ -15,4 +18,24 @@ defmodule Advent1 do
     |> Stream.map( &(String.replace(&1, "\n", "")) )
     |> Enum.map(&String.to_integer/1)
   end
+
+
+  ############
+  # PART 2
+  ############
+  def part2 do
+
+    calculateFuel(12)
+
+  end
+
+  def calculateFuel(mass) do
+
+    m = mass / 3
+    m = Float.floor(m, 0)
+    m = m - 2
+
+  end
+
+
 end
