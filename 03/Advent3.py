3b7444cd8c4b38703280912bcd1b1dc596486450
RAG <ragiroux@gmail.com>
2019-12-04 23:07:49 -0500

    test python

--- 03/Advent3.py
+++ 03/Advent3.py
@@ -7,14 +7,33 @@ def main():
 
   cableCoordinate1 = []
   cableCoordinate2 = []
+  dest = []
+
+  xyz(cable1, Point(0,0), 0, dest)
 
   for c in cable1:
-    cableCoordinate1.append(addToPointList(c))
+    cableCoordinate1.append(addToPointList(c)) 
 
   for c in cable2:
     cableCoordinate2.append(addToPointList(c))
 
   print(cableCoordinate2)
+
+def xyz(list, previous, next, dest):
+
+    op = list[next][0]
+
+    if op == 'R':
+        dest.append(Point(int(list[next][1:]) + previous.getX(), 0 + previous.getY()))
+    if op == 'L':
+        dest.append(Point(-int(list[next][1:]) + previous.getX(), 0 + previous.getY()))
+    if op == 'U':
+        dest.append(Point(0 + previous.getX(), int(list[next][1:] + previous.getY())))
+    if op == 'D':
+         dest.append(Point(0 + previous.getX(), -int(list[next][1:] + previous.getY())))
+
+    xyz(list,dest[-1],next+1, dest)      
+
 
 def addToPointList(cablePoint):
     op = cablePoint[0]
